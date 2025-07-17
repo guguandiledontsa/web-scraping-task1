13af84829257a79d1408bbe1431fff9df6564a12
Gugu (Andile) Dontsa <211983812+guguandiledontsa@users.noreply.github.com>
2025-07-18 01:54:30 +0200

    Update parser.py

--- src/main/parser.py
+++ src/main/parser.py
@@ -1,6 +1,10 @@
-# parser.py
 from bs4 import BeautifulSoup
+from utils import get_logger
+
+logger = get_logger()
 
 def extract_titles(response):
     soup = BeautifulSoup(response.text, "lxml")
-    return [tag['title'] for tag in soup.select("h3 a")]
+    titles = [a_tag.text.strip() for a_tag in soup.select("h3 a")]
+    logger.info(f"Extracted {len(titles)} titles")
+    return titles
