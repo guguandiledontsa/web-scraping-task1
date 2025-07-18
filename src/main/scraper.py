869bfce91f7d83fa1a589c1c2aae5c99a18d5e7d
Gugu (Andile) Dontsa <211983812+guguandiledontsa@users.noreply.github.com>
2025-07-19 01:15:31 +0200

    Update scraper.py

--- src/main/scraper.py
+++ src/main/scraper.py
@@ -1,5 +1,6 @@
 """module to handle request logic."""
 import requests
+from bs4 import BeautifulSoup
 from .utils import get_logger
 
 logger = get_logger()
@@ -15,3 +16,9 @@ def fetch_response(url, session=None, timeout=10):
     except requests.exceptions.RequestException as e:
         logger.error(f"Failed to fetch {url}: {e}")
         return None
+
+def fetch_soup(url, session=None, timeout=10, parser="lxml"):
+    """Use fetch_response to make soup from url response."""
+    response = fetch_response(url, session,timeout)
+    soup = BeautifulSoup(response.text, parser)
+    return soup
