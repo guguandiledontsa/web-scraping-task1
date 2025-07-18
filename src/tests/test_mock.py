3d94cde85f71d249963a99072919bbcbccfab697
Gugu (Andile) Dontsa <211983812+guguandiledontsa@users.noreply.github.com>
2025-07-18 03:21:28 +0200

    Create test_mock.py

---
+++ src/tests/test_mock.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import Mock
+from main.parser import extract_titles
+
+class TestExtractTitles(unittest.TestCase):
+    def test_extract_titles(self):
+        html = '''
+        <html>
+          <body>
+            <h3><a title="Title 1">Link 1</a></h3>
+            <h3><a title="Title 2">Link 2</a></h3>
+            <h3><a>No title here</a></h3>
+          </body>
+        </html>
+        '''
+        mock_response = Mock()
+        mock_response.text = html
+
+        titles = extract_titles(mock_response)
+        self.assertEqual(titles, ["Title 1", "Title 2"])
+
+if __name__ == "__main__":
+    unittest.main()
