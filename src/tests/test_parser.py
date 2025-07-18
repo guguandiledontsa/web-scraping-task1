ed08233e712c9b69cf50ef6b97ec29645b1ae383
Gugu (Andile) Dontsa <211983812+guguandiledontsa@users.noreply.github.com>
2025-07-18 06:11:07 +0200

    Update test_parser.py

--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock
-from main.parser import extract_titles
+from main.parser import extract_elements
 
 class TestExtractTitles(unittest.TestCase):
     def test_extract_titles(self):
